f92deb605454270872ecabb08ca29a5919a7b72b
Arun Devabhaktuni <adevabhaktuni@gmail.com>
2013-02-14 00:10:30 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoccerRoster::Application.config.secret_token = '3e773a6fd4179df51503a80b59fd8458fffe3b5e7761ff83d9864d12e8bdececfe84521f2c2c6ce0c0235c2ecdccc61791fe8bbb085f38390c1fc7605d598b93'
